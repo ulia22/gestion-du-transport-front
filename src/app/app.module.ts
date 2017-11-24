f410f507a22e3a51cc70e47f44f8eb65c065cc39
Clément BARBASTE <barbaste.clement@gmail.com>
2017-11-24 17:32:54 +0100

    Ajout du routeur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule }  from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes: Routes = [
+{ path: 'app', component: AppComponent }, // /page1 affiche le composant A
+{ path: '**', redirectTo: 'app'} // redirige vers la route page1 par défaut
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     NgbModule.forRoot(),
     BrowserModule
   ],
