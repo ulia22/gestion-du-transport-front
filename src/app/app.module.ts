60138c4eb2348624093ec9128720fe441bc4519a
Ange STOIANOV <stoianov.ange@hotmail.fr>
2017-11-28 15:02:30 +0100

    nouveau navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { ListeAnnonceComponent } from './liste-annonce/liste-annonce.component';
 
 const appRoutes: Routes = [
 //{ path: 'app', component: AppComponent }, // /page1 affiche le composant A
 { path: 'collaborateur/reservations', component: AppComponent },
-{ path: 'collaborateur/annonces', component: AppComponent },
+{ path: 'collaborateur/annonces', component: ListeAnnonceComponent },
 { path: 'collaborateur/statistiques', component: AppComponent },
 { path: 'admin/vehicules', component: AppComponent },
-{ path: 'admin/chauffeur', component: AppComponent },
+{ path: 'admin/chauffeurs', component: AppComponent },
 
 { path: 'chauffeur/occupation', component: AppComponent },
 { path: 'chauffeur/planning', component: AppComponent }//,
@@ -23,7 +24,8 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ListeAnnonceComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
